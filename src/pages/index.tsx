8a7fdcf7c9c5f5acfbd9dbe86b3fb1419fe3c346
Orangeman7201 <aki.jazz1027@gmail.com>
2024-02-23 18:27:17 +0900

    灰色の部分をなくす

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
 import { Inter } from "next/font/google";
-import { useEffect, useState } from 'react';
 import Link from 'next/link'
-import axios from '../plugins/axios';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [conditions, setConditions] = useState([]);
-  useEffect(() => {
-    axios.get('http://localhost:3000/api/v1/conditions').then((response) => {
-      setConditions(response.data);
-    });
-  }, [])
-
-  // conditionsがnullの場合はnullを返却
-  if (!conditions.length) return null;
-
   return (
     <div>
       <div>トップ画面です</div>
