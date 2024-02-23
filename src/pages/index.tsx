5468bf86d628d1dc8e154fd0861b0a215f6e79d8
Orangeman7201 <aki.jazz1027@gmail.com>
2024-02-23 19:04:41 +0900

    cssをそれなりに揃える

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <div>トップ画面です</div>
-      <Link href="/conditions">体調一覧</Link>
+      <div className="mb-8">トップ画面です</div>
+      <Link href="/conditions" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">体調一覧</Link>
     </div>
   );
 }
