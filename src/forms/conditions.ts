9ce2f2a2fddbd91edf028655cb48c7650348f4d9
Orangeman7201 <aki.jazz1027@gmail.com>
2024-04-14 20:45:19 +0900

    メモと強さを入力できるようにする

--- src/forms/conditions.ts
+++ src/forms/conditions.ts
@@ -2,10 +2,14 @@ export default class Condition {
   id: number;
   detail: string;
   occurredDate: string; // format: 'YYYY-MM-DD, formまではdayjsで扱う。'
+  strength: number;
+  memo: string;
 
-  constructor(data?: { id?: number, detail?: string, occurredDate?: string }) {
+  constructor(data?: { id?: number, detail?: string, occurredDate?: string, strength?: number, memo?: string}) {
     this.id = data?.id || 0;
     this.detail = data?.detail || '';
     this.occurredDate = data?.occurredDate || '';
+    this.strength = data?.strength || 0;
+    this.memo = data?.memo || '';
   }
 }
